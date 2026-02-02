dfcdb328579c84524f957f454f6e8d460accdba7
Venkatesh <venkateshkumar.mandla@tekworks.in>
2026-02-02 17:58:33 +0530

    config data changed

--- src/api/config.ts
+++ src/api/config.ts
@@ -4,7 +4,7 @@ import { Capacitor } from '@capacitor/core';
 console.log('🔌 [Config] Platform:', Capacitor.getPlatform());
 console.log('📱 [Config] Is Native:', Capacitor.isNativePlatform());
 
-const USE_LOCAL_IP = true;
+const USE_LOCAL_IP = false;
 // const LOCAL_IP_ADDRESS = "192.168.200.162"; // Your local IP
 const LOCAL_IP_ADDRESS = "localhost"; // Your local IP
 
